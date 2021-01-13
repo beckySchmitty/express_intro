1324df1e769562b1dd9548ab558c7691a5361b3a
Becky Schmitthenner <becky.schmitthenner@gmail.com>
2021-01-13 18:38:25 -0500

    res/req notes

--- routes.js
+++ routes.js
@@ -11,14 +11,38 @@ app.get('/dogs', function(req, res) {
 })
 
 app.get('/cats', (req, res) => {
-    return res.send("GET request for cats")
+    return res.send("This is the GET request for /cats")
 })
 app.post('/cats', function addCat (req, res) {
     return res.send("Added a new cat, jk, this is a test")
 })
 
 
+app.get('/greet/:lang', function (req, res) {
+    let greet = req.params.lang
+    let greeting = greetings[greet]
+    if (!greeting) return res.send("Invalid Language")
+    return res.send(greeting)
+})
+
+app.get('/search', function (req, res) {
+    const {animal = 'cat', count = '9' } = req.query;
+    return res.send(`You searched: Animal = ${animal}, Count = ${count}`)
+})
+
+app.get('/show-language', (req, res) => {
+    let lang = req.headers['accept-language']
+    return res.send(`Your language preference is set to: ${lang}`)
+})
+
 // should always be at the bottom of a file
 app.listen(3000, function() {
     console.log('App on port 3000');
 })
+
+const greetings = {
+    en: "hello",
+    fr: "bonjour",
+    sp: "hola",
+    ic: "hallo",
+}
