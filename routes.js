27cd12d675ee81b5b5795a34fd903e99964e0188
Becky Schmitthenner <becky.schmitthenner@gmail.com>
2021-01-14 10:12:54 -0500

    req.body notes

--- routes.js
+++ routes.js
@@ -1,6 +1,13 @@
 const express = require('express');
-
 const app = express();
+
+// so we can use req.body
+app.use(express.json())
+// so we can access form data
+app.use(express.urlencoded({ extended: true}));
+
+
+
 
 app.get('/', function (req, res) {
     return res.send('Home!')
@@ -28,6 +35,11 @@ app.get('/greet/:lang', function (req, res) {
 app.get('/search', function (req, res) {
     const {animal = 'cat', count = '9' } = req.query;
     return res.send(`You searched: Animal = ${animal}, Count = ${count}`)
+})
+
+// added app.use above - check this w/ insomnia
+app.post('/register', function (req, res) {
+    res.send(`Welcome, ${req.body.username}`)
 })
 
 app.get('/show-language', (req, res) => {
